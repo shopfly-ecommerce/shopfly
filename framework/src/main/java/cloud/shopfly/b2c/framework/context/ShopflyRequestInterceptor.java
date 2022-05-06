49ed445f4277dc296c99f30baa473d7e46df6abf
kingapex@javashop.cn <kingapex@javashop.cn>
2022-05-06 19:43:09 +0800

    add copyright

--- framework/src/main/java/cloud/shopfly/b2c/framework/context/ShopflyRequestInterceptor.java
+++ framework/src/main/java/cloud/shopfly/b2c/framework/context/ShopflyRequestInterceptor.java
@@ -1,8 +1,18 @@
 /*
- * 易族智汇（北京）科技有限公司 版权所有。
- * 未经许可，您不得使用此文件。
- * 官方地址：www.javamall.com.cn
-*/
+ *  Copyright 2008-2022 Shopfly.cloud Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package cloud.shopfly.b2c.framework.context;
 
 import javax.servlet.http.HttpServletRequest;
